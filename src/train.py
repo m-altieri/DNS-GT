4bd49140724622ef1a77e367f67cc7e4ded9c2de
altiema <massimiliano.altieri@ec.europa.eu>
2023-01-24 15:24:33 +0100

    .

--- src/train.py
+++ src/train.py
@@ -5,7 +5,8 @@ import tensorflow as tf
 
 physical_devices = tf.config.list_physical_devices("GPU")
 try:
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+    for device in physical_devices:
+        tf.config.experimental.set_memory_growth(device, True)
 except:
     print("Cannot enable memory growth on first physical device.")
     sys.exit(1)

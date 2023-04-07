4c5dc4459f800779d18d9a5bc79041fd672720d2
altiema <massimiliano.altieri@ec.europa.eu>
2023-04-07 13:25:12 +0200

    improved create_vocabs.py

--- src/preprocessing/create_vocabs.py
+++ src/preprocessing/create_vocabs.py
@@ -2,59 +2,157 @@ import numpy as np
 import os
 import argparse
 import re
+import sys
+
 
 argparser = argparse.ArgumentParser()
-argparser.add_argument('--hosts_folder', default='arrays/small/hosts/', help='Folder containing the hosts arrays')
-argparser.add_argument('--domains_folder', default='arrays/small/domains/', help='Folder containing the domains arrays')
-argparser.add_argument('--output_folder', default='vocabs/small/', help='Folder to output the vocabularies to')
+argparser.add_argument(
+    "--hosts_folder",
+    default="arrays/all/hosts/",
+    help="Folder containing the hosts arrays",
+)
+argparser.add_argument(
+    "--domains_folder",
+    default="arrays/all/domains/",
+    help="Folder containing the domains arrays",
+)
+argparser.add_argument("--queries_folder", default="arrays/all/queries/train")
+argparser.add_argument(
+    "--output_folder",
+    default="vocabs/all/exp/",
+    help="Folder to output the vocabularies to",
+)
 args = argparser.parse_args()
 
 hosts, domains = [], []
 
-for f in os.listdir(args.hosts_folder):
-    fpath = os.path.join(args.hosts_folder, f)
-    new_hosts = np.load(fpath, allow_pickle=True, encoding='ASCII')
-    #for i in range(len(new_hosts)):
-    #    new_hosts[i] = re.sub('[^!-~]+', '', new_hosts[i]).lower()
-    #    match = re.match('^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$', new_hosts[i])
-    #    if match:
-    #        new_hosts[i] = match[0]
-    new_hosts = list(map(lambda h: re.sub('[^!-~\\n]+', '', h), new_hosts))
-    hosts = np.unique(np.concatenate((hosts, new_hosts)))
-    print(f'{f} processed')
-hosts = np.unique(hosts)
-    
-for f in os.listdir(args.domains_folder):
-    fpath = os.path.join(args.domains_folder, f)
-    new_domains = np.load(fpath, allow_pickle=True, encoding='ASCII')
-    #for i in range(len(new_domains)):
-    #    new_domains[i] = re.sub('[^!-~]+', '', new_domains[i]).lower()
-    #    match = re.match('^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$', new_domains[i])
-    #    if match:
-    #        new_domains[i] = match[0]
-    new_domains = list(map(lambda d: re.sub('[^!-~\\n]+', '', d), new_domains))
-    domains = np.unique(np.concatenate((domains, new_domains)))
-    print(f'{f} processed')
-domains = np.unique(domains)
+for f in os.listdir(args.queries_folder):
+    if f == "count.py":
+        continue
+    q = np.load(os.path.join(args.queries_folder, f), allow_pickle=True)
+    hosts = np.concatenate([hosts, q[:, 0]])
+    domains = np.concatenate([domains, q[:, 1]])
+    print(f"{f} loaded")
 
-hosts_vocab = '\n'.join(hosts)#.replace('[', '').replace(']', '')#.encode('ascii', errors='ignore').decode()
-domains_vocab = '\n'.join(domains)#.replace('[', '').replace(']', '')#.encode('ascii', errors='ignore').decode()
+hosts = list(map(lambda h: re.sub("[^!-~\\n]+", "", h), hosts))
+hosts = np.unique(hosts, return_counts=True)
+sorter = np.argsort(hosts[1])[::-1]
+hosts[0][:] = hosts[0][sorter]
+hosts[1][:] = hosts[1][sorter]
+print("Hosts processed")
 
-#hosts_vocab = re.sub('[^!-~\\n]+', '', hosts_vocab)
-#domains_vocab = re.sub('[^!-~\\n]+', '', domains_vocab)
+domains = list(map(lambda d: re.sub("[^!-~\\n]+", "", d), domains))
+domains = np.unique(domains, return_counts=True)
+sorter = np.argsort(domains[1])[::-1]
+domains[0][:] = domains[0][sorter]
+domains[1][:] = domains[1][sorter]
+print("Domains processed")
+
+hosts_vocab = "\n".join(
+    hosts[0]
+)  # .replace('[', '').replace(']', '')#.encode('ascii', errors='ignore').decode()
+domains_vocab = "\n".join(
+    domains[0]
+)  # .replace('[', '').replace(']', '')#.encode('ascii', errors='ignore').decode()
 
 if not os.path.exists(args.output_folder):
     os.makedirs(args.output_folder)
 
-with open(os.path.join(args.output_folder, 'hosts_vocab.txt'), 'w') as f:
-    f.write('<START>\n')
-    f.write('<PAD>\n')
-    f.write('<MASK>\n')
+hosts_vocab_path = os.path.join(args.output_folder, "hosts_vocab.txt")
+with open(hosts_vocab_path, "w") as f:
+    f.write("<START>\n")
+    f.write("<PAD>\n")
+    f.write("<MASK>\n")
+    f.write("<UNK>\n")
     f.write(hosts_vocab)
+print("Hosts vocab saved in", hosts_vocab_path)
 
-with open(os.path.join(args.output_folder, 'domains_vocab.txt'), 'w') as f:
-    f.write('<START>\n')
-    f.write('<PAD>\n')
-    f.write('<MASK>\n')
+domains_vocab_path = os.path.join(args.output_folder, "domains_vocab.txt")
+with open(domains_vocab_path, "w") as f:
+    f.write("<START>\n")
+    f.write("<PAD>\n")
+    f.write("<MASK>\n")
+    f.write("<UNK>\n")
     f.write(domains_vocab)
-    
+print("Domains vocab saved in", domains_vocab_path)
+
+sys.exit(0)
+
+for f in os.listdir(args.hosts_folder):
+    new_hosts = np.load(
+        os.path.join(args.hosts_folder, f), allow_pickle=True, encoding="ASCII"
+    )
+    hosts = np.concatenate([hosts, new_hosts])
+    print(f"{f} loaded")
+
+hosts = list(map(lambda h: re.sub("[^!-~\\n]+", "", h), hosts))
+
+hosts = np.unique(hosts, return_counts=True)
+sorter = np.argsort(hosts[1])[::-1]
+hosts[0][:] = hosts[0][sorter]
+hosts[1][:] = hosts[1][sorter]
+print("Hosts processed")
+
+# for f in os.listdir(args.hosts_folder):
+#     fpath = os.path.join(args.hosts_folder, f)
+#     new_hosts = np.load(fpath, allow_pickle=True, encoding="ASCII")
+#     print(len(new_hosts))
+#     new_hosts = list(map(lambda h: re.sub("[^!-~\\n]+", "", h), new_hosts))
+#     new_hosts = np.unique(new_hosts, return_counts=True)
+#     sorter = np.argsort(new_hosts[1][:])
+#     new_hosts[0][:] = new_hosts[0][sorter]
+#     new_hosts[1][:] = new_hosts[1][sorter]
+#     print(new_hosts)
+#     print(np.max(new_hosts[1]))
+#     print(len(new_hosts[0]))
+
+#     continue
+#     sys.exit()
+
+#     new_hosts = list(map(lambda h: re.sub("[^!-~\\n]+", "", h), new_hosts))
+#     hosts = np.unique(np.concatenate((hosts, new_hosts)))
+# hosts = np.unique(hosts)
+
+for f in os.listdir(args.domains_folder):
+    new_domains = np.load(
+        os.path.join(args.domains_folder, f), allow_pickle=True, encoding="ASCII"
+    )
+    domains = np.concatenate([domains, new_domains])
+    print(f"{f} loaded")
+
+domains = list(map(lambda d: re.sub("[^!-~\\n]+", "", d), domains))
+
+domains = np.unique(domains, return_counts=True)
+sorter = np.argsort(domains[1])[::-1]
+domains[0][:] = domains[0][sorter]
+domains[1][:] = domains[1][sorter]
+print("Domains processed")
+
+hosts_vocab = "\n".join(
+    hosts[0]
+)  # .replace('[', '').replace(']', '')#.encode('ascii', errors='ignore').decode()
+domains_vocab = "\n".join(
+    domains[0]
+)  # .replace('[', '').replace(']', '')#.encode('ascii', errors='ignore').decode()
+
+# hosts_vocab = re.sub('[^!-~\\n]+', '', hosts_vocab)
+# domains_vocab = re.sub('[^!-~\\n]+', '', domains_vocab)
+
+if not os.path.exists(args.output_folder):
+    os.makedirs(args.output_folder)
+
+with open(os.path.join(args.output_folder, "hosts_vocab.txt"), "w") as f:
+    f.write("<START>\n")
+    f.write("<PAD>\n")
+    f.write("<MASK>\n")
+    f.write("<UNK>\n")
+    f.write(hosts_vocab)
+print("Hosts vocab saved")
+
+with open(os.path.join(args.output_folder, "domains_vocab.txt"), "w") as f:
+    f.write("<START>\n")
+    f.write("<PAD>\n")
+    f.write("<MASK>\n")
+    f.write("<UNK>\n")
+    f.write(domains_vocab)
+print("Domains vocab saved")
--- src/preprocessing/csv2arrays.py
+++ src/preprocessing/csv2arrays.py
@@ -5,39 +5,47 @@ import argparse
 import re
 
 argparser = argparse.ArgumentParser()
-argparser.add_argument('input_folder', help='Folder containing processed PCAPs in .csv format')
-argparser.add_argument('output_folder', help='Folder to save output arrays to')
-argparser.add_argument('--from-responses', action='store_const', const=True, default=False, help='Whether to extract query information from requests or responses. If True and some queries have no response, it will throw an error.')
+argparser.add_argument(
+    "input_folder", help="Folder containing processed PCAPs in .csv format"
+)
+argparser.add_argument("output_folder", help="Folder to save output arrays to")
+argparser.add_argument(
+    "--from-responses",
+    action="store_const",
+    const=True,
+    default=False,
+    help="Whether to extract query information from requests or responses. If True and some queries have no response, it will throw an error.",
+)
 args = argparser.parse_args()
 
 
 def clean_urls(url):
-    url = re.sub('[^!-~]+', '', url).lower()
-    #match = re.match('^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$', url)
+    url = re.sub("[^!-~]+", "", url).lower()
+    # match = re.match('^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$', url)
     #    if match:
     #        url = match[0]
     return url
-    
-    
+
+
 # check input folder is valid
 if not os.path.exists(args.input_folder) or len(os.listdir(args.input_folder)) == 0:
-    raise FileNotFoundError(f'{args.input_folder} is not a valid input folder.')
+    raise FileNotFoundError(f"{args.input_folder} is not a valid input folder.")
 
 # create output folder if not exists
-if not os.path.exists(os.path.join(args.output_folder, 'hosts')):
-    os.makedirs(os.path.join(args.output_folder, 'hosts'))
-if not os.path.exists(os.path.join(args.output_folder, 'domains')):
-    os.makedirs(os.path.join(args.output_folder, 'domains'))
-if not os.path.exists(os.path.join(args.output_folder, 'queries')):
-    os.makedirs(os.path.join(args.output_folder, 'queries'))
+if not os.path.exists(os.path.join(args.output_folder, "hosts")):
+    os.makedirs(os.path.join(args.output_folder, "hosts"))
+if not os.path.exists(os.path.join(args.output_folder, "domains")):
+    os.makedirs(os.path.join(args.output_folder, "domains"))
+if not os.path.exists(os.path.join(args.output_folder, "queries")):
+    os.makedirs(os.path.join(args.output_folder, "queries"))
 
 for filename in os.listdir(args.input_folder):
     # read processed pcap
     df = pd.read_csv(os.path.join(args.input_folder, filename))
 
     # get host and domain columns
-    hosts = df['req_src_ip'] if not args.from_responses else df['res_dst_ip']
-    domains = df['req_qry_domain'] if not args.from_responses else df['res_qry_domain']
+    hosts = df["req_src_ip"] if not args.from_responses else df["res_dst_ip"]
+    domains = df["req_qry_domain"] if not args.from_responses else df["res_qry_domain"]
 
     # remove NaNs (responses with no query)
     hosts = hosts.dropna()
@@ -47,15 +55,32 @@ for filename in os.listdir(args.input_folder):
     assert all(domains.str.startswith("b'")) or not any(domains.str.startswith("b'"))
     if all(domains.str.startswith("b'")):
         domains = domains.map(lambda x: str(x).split("'")[1])
-        
+
     hosts = hosts.map(clean_urls)
     domains = domains.map(clean_urls)
 
     # save array of communications
-    np.save(os.path.join(args.output_folder, 'queries', f'queries-{filename}.npy'), list(zip(hosts, domains)))
+    np.save(
+        os.path.join(args.output_folder, "queries", f"queries-{filename}.npy"),
+        list(zip(hosts, domains)),
+    )
 
     # save arrays of unique hosts and domains
-    np.save(os.path.join(args.output_folder, 'hosts', f'hosts-{filename}.npy'), hosts.unique())
-    np.save(os.path.join(args.output_folder, 'domains', f'domains-{filename}.npy'), domains.unique())
-
-
+    np.save(
+        os.path.join(args.output_folder, "hosts", f"hosts-nonunique-{filename}.npy"),
+        hosts,
+    )
+    np.save(
+        os.path.join(
+            args.output_folder, "domains", f"domains-nonunique-{filename}.npy"
+        ),
+        domains,
+    )
+    np.save(
+        os.path.join(args.output_folder, "hosts", f"hosts-{filename}.npy"),
+        hosts.unique(),
+    )
+    np.save(
+        os.path.join(args.output_folder, "domains", f"domains-{filename}.npy"),
+        domains.unique(),
+    )
